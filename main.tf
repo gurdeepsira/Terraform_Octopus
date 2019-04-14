9c9b2c8d5bf5fbe0c60be0ed45c2d0aa6e10fe2d
Administrator <gurdipsira_1@yahoo.co.uk>
2019-04-14 14:02:29 -0700

    xc

--- main.tf
+++ main.tf
@@ -17,3 +17,18 @@ resource "octopusdeploy_project" "BuildOverview ASP.NET Project" {
   project_group_id      = "${octopusdeploy_project_group.finance.id}"
   skip_machine_behavior = "SkipUnavailableMachines"
 }
+
+
+resource "octopusdeploy_machine" "Production" {
+  name                            = "GLOVSIISD01"
+  environments                    = ["${data.octopusdeploy_environment.staging.id}"]
+  isdisabled                      = false
+  machinepolicy                   = "${data.octopusdeploy_machinepolicy.default.id}"
+  roles                           = ["Production"]
+  tenanteddeploymentparticipation = "Untenanted"
+
+  endpoint {
+    communicationstyle = "TentaclePassive"
+    thumbprint         = "81D0FF8B76FC"
+    uri                = "https://finance-web-01:10933"
+  }
